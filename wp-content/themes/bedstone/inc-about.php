9b85c19b3772ba9b77e2d6825bcc99d969567078
Tessa Kriesel <tessak22@outlook.com>
2015-05-27 09:29:18 -0500

    Full Dev Progress

--- wp-content/themes/bedstone/inc-about.php
+++ wp-content/themes/bedstone/inc-about.php
@@ -11,9 +11,9 @@ $mypages = new WP_Query(array(
 ?>
 
 <?php while ($mypages->have_posts()) : $mypages->the_post(); ?>
-    <div class="child-item row">
+    <div class="child-item">
         <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_excerpt(); ?>
-        <p class="call-to-action"><a href="<?php the_permalink(); ?>">Read More</a></p>
+        <p class="call-to-action"><a href="<?php the_permalink(); ?>"><?php the_field('call_to_action_link_text'); ?></a></p>
     </div>
 <?php endwhile; wp_reset_postdata(); ?>
