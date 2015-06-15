554c3c153a257bd95b985e23ee36dfc5177bbba5
Tessa Kriesel <tessak22@outlook.com>
2015-06-15 13:46:09 -0500

    QA Fixes part 1

---
+++ wp-content/themes/bedstone/home.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * default catch all
+ *
+ * author
+ * category
+ * custom post type archive
+ * custom taxonomy archive
+ * date archive -- year, month, day
+ * search results
+ * tag archive
+ *
+ * @package Bedstone
+ */
+
+get_header(); ?>
+
+<div class="container-columns row">
+    <div class="content content-list col-md-8" role="main">
+        <header class="document-header">
+            <h1>
+                <?php
+                if (is_category()) {
+                    single_cat_title();
+                } elseif (is_tag()) {
+                    single_tag_title();
+                } elseif (is_author()) {
+                    echo 'Author: ' . get_the_author();
+                } elseif (is_day()) {
+                    echo 'Archive: ' . get_the_date('l, F j, Y');
+                } elseif (is_month()) {
+                    echo 'Archive: ' . get_the_date('j Y');
+                } elseif (is_year()) {
+                    echo 'Archive: ' . get_the_date('Y');
+                } elseif (is_search()) {
+                    echo 'Results for: ' . get_search_query();
+                } elseif (is_home() && is_front_page()) {
+                    // Settings > Reading > Front Page Displays > Your Latest Posts
+                    echo get_bloginfo('name');
+                } elseif (is_home()) {
+                    // Settings > Reading > Front Page Displays > Static Page > Posts Page
+                    echo get_the_title(get_option('page_for_posts', true));
+                } else {
+                    echo 'Archives';
+                }
+                ?>
+            </h1>
+        </header>
+
+        <div class="row">
+            <div class="col-md-12" role="main">
+                <?php setup_postdata($GLOBALS['post'] =& get_post(11)); ?>
+                    <?php the_content(); ?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+        </div>
+
+        <?php
+        if (is_search()) {
+            get_search_form();
+        }
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                get_template_part('content', 'list');
+            }
+            if (is_search()) {
+                get_search_form();
+            }
+            get_template_part('nav', 'archive');
+        } else {
+            get_template_part('content', 'none');
+        }
+        ?>
+    </div>
+    <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/bedstone/inc-about.php
+++ wp-content/themes/bedstone/inc-about.php
@@ -12,7 +12,7 @@ $mypages = new WP_Query(array(
 
 <?php while ($mypages->have_posts()) : $mypages->the_post(); ?>
     <div class="child-item">
-        <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><?php the_title(); ?></h2>
         <?php the_excerpt(); ?>
         <p class="call-to-action"><a href="<?php the_permalink(); ?>"><?php the_field('call_to_action_link_text'); ?></a></p>
     </div>
